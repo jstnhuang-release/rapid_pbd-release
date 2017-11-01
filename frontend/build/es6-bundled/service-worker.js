ca8539a6ebbc8deddf6783586743dff546b255bd
Justin Huang <jstn@cs.washington.edu>
2017-10-31 19:22:54 -0700

    Rebase from 'rpm/indigo/rapid_pbd'

--- frontend/build/es6-bundled/service-worker.js
+++ frontend/build/es6-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","0a7d36e1575b70f8dd026043cce23cb4"]];
+var precacheConfig = [["index.html","85b9034a2bc51bd2d98734b1a00e6e61"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
